a7d325aa17a98a66b16081a60a7abc338ee0e2c1
DevSrivastava02 <Srivastavaharsh427@gmail.com>
2025-05-01 11:41:49 +0530

    Varibale Initilized:

---
+++ app/src/main/java/com/example/kotlinbasic/Hello_Kotlin.kt
@@ -0,0 +1,23 @@
+package com.example.kotlinbasic
+
+fun main(){
+    // The single line comment done with the help of (//) this
+    /*
+     This help to for multiline comment that
+     help to know what will perform in the aur code
+     */
+
+    println("Hello Kotlin, this is my first step beacome the king")
+
+    /*
+    There are two of variable in android studio
+    1> immutable --> that is create with the help of (val variable_name:data_type=value)
+                       the immutable variable can't be change
+    2> mutable --> that is create with the help of (var variable_name:data_type=value)
+                        the value can be change according to their need
+     */
+    val pi:Double=3.14
+    var age:Int=94
+    println("The value of pi is:"+pi)
+    println("The value of age is:"+age)
+}
